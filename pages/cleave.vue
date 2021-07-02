631f92e861729a362d74394efe20ab9cb9249de2
AhmedNiazy309 <ahmedalsolya@gmail.com>
2021-07-02 17:15:15 -0400

    Dashboard

---
+++ pages/cleave.vue
@@ -0,0 +1,51 @@
+<template>
+ <div>
+ 
+ <input type="text" class="scroll-target" id="scroll-target">
+ </div>
+
+</template>
+
+<script>
+
+    import Cleave from 'cleave.js'
+
+
+export default {
+  layout(context) {
+    return 'default2'
+  },
+        mounted(){
+    setTimeout(function () {
+      console.log(document.querySelector('#scroll-target'));
+
+
+
+const cleave = new Cleave('.scroll-target', {
+    date: true,
+    delimiter: '-',
+    datePattern: ['Y', 'm', 'd']
+});
+
+
+
+
+
+
+    }.bind(this), 2000);
+        }
+    }
+
+
+    /*
+    
+    var cleave = new Cleave('.input-element', {
+    date: true,
+    delimiter: '-',
+    datePattern: ['Y', 'm', 'd']
+});
+    
+    
+    */
+</script>
+
